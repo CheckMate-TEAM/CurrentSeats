4cf7587aeaedec93b7d6f5ae75ec9413bad35fb4
ChiKim22 <wlsgh2505@gmail.com>
2021-06-02 20:39:15 +0900

    Seat Layout (Dialog)

--- app/src/main/java/com/example/currentseats/MainActivity.java
+++ app/src/main/java/com/example/currentseats/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.currentseats;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DialogFragment;
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.fonts.FontFamily;
+import android.graphics.fonts.FontStyle;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.Gravity;
@@ -13,40 +20,43 @@ import android.view.Window;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 //    static GridLayout.Spec spec(int start[, int size, GridLayout.Alignment ali]) {
 //        return null;
 //    }
 
     ViewGroup layout;
 
-    String seats = "_NNNN___NNN__/"+
-                     "_NNNN___NNN__/"+
-                     "_NNNN___NNN__/"+
-                     "_NNNN___NNN__/"+
-                     "_NNNN___NNN__/"+
-                     "_NNNN___NNN__/"+
-                     "_NNNN___NNNN_/";
+
+    // 좌석 배치
+    String seats =   "NNNN__NNN__/"+
+                     "NNNN__NNN__/"+
+                     "NNNN__NNN__/"+
+                     "NNNN__NNN__/"+
+                     "NNNN__NNN__/"+
+                     "NNNN__NNN__/"+
+                     "NNNN__NNNN_/";
+
 
     List<TextView> seatView = new ArrayList<>();
-    int seatSize = 65;
+    int seatWidth = 130;
+    int seatHeight = 150;
     int seatGaping = 10;
-    int STATUS_NORMAL = 1;
-    int STATUS_EMPTY = 2;
     int cnt = 0;
-
-
+    int i;
+    TextView tv1, tv2;
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 //        requestWindowFeature(Window.FEATURE_NO_TITLE);
-
-        setContentView(R.layout.popup_activity);
 
         layout = findViewById(R.id.layoutSeats);
 
@@ -57,57 +67,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                                                         ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutSeat.setOrientation(LinearLayout.VERTICAL);
         layoutSeat.setLayoutParams(params);
-        layoutSeat.setPadding(8 * seatGaping, 8 * seatGaping, 8 * seatGaping, 8 * seatGaping);
+        layoutSeat.setPadding(8 * seatGaping, 30 * seatGaping, 8 * seatGaping, 8 * seatGaping);
         layout.addView(layoutSeat);
 
         LinearLayout layout = null;
+//        //dialog
+//        OptionCodeTypeDialog customDialog = new OptionCodeTypeDialog(this, close);
 
 
-        for(int i = 0; i < seats.length(); i++){
-            if(seats.charAt(i) == '/'){
+        // 시트 구성
+        for(i = 0; i < seats.length(); i++) {
+            if (seats.charAt(i) == '/') {
                 layout = new LinearLayout(this);
                 layout.setOrientation(LinearLayout.HORIZONTAL);
                 layoutSeat.addView(layout);
-            }else if(seats.charAt(i) == 'N'){
+
+
+            } else if (seats.charAt(i) == 'N') { // 좌석 부분
+                tv1 = new TextView(this);
                 cnt++;
-                TextView tv = new TextView(this);
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatWidth, seatHeight);
                 layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
-                tv.setLayoutParams(layoutParams);
-                tv.setPadding(0, 0, 0, 2 * seatGaping);
-                tv.setId(cnt);
-//                tv.setGravity(Gravity.CENTER);
-//                tv.setBackgroundResource(TypedValue.COMPLEX_UNIT_DIP, 9);
-                tv.setText(cnt + "");
-                tv.setTextColor(Color.BLACK);
-                tv.setTextSize(15);
-                layout.addView(tv);
-                seatView.add(tv);
-                tv.setTag("STATUS_NORMAL");
-                tv.setOnClickListener(this);
-            }else if(seats.charAt(i) == '_'){
-                TextView tv = new TextView(this);
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                tv1.setLayoutParams(layoutParams);
+                tv1.setPadding(0, 0, 0, 0);
+                tv1.setId(cnt);
+                tv1.setGravity(Gravity.CENTER);
+                tv1.setText(cnt + "");
+                tv1.getAutoSizeMaxTextSize();
+                tv1.setTextColor(Color.BLACK);
+                layout.addView(tv1);
+                seatView.add(tv1);
+                tv1.setBackgroundResource(R.drawable.seats);
+                tv1.setOnClickListener(this); // 팝업창
+
+
+            } else if (seats.charAt(i) == '_') { // 공백부분
+                tv2 = new TextView(this);
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatWidth, seatHeight);
                 layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
-                tv.setLayoutParams(layoutParams);
-                tv.setBackgroundColor(Color.TRANSPARENT);
-                tv.setText("");
-                layout.addView(tv);
+                tv2.setLayoutParams(layoutParams);
+                tv2.setBackgroundColor(Color.TRANSPARENT);
+                tv2.setText("");
+                layout.addView(tv2);
             }
         }
-
-
     }
+
     @Override
-    public void onClick(View view){
-        if((int) tv.getTag() == STATUS_NORMAL){
-            Intent intent = new Intent();
-            intent.putExtra("data", "test Popup");
-            startActivityForResult(intent, cnt);
-
-            finish();
-//            view.popup();
-        }
-
+    public void onClick(View v) {
+        CustomDialog dialog = new CustomDialog(this);
+        dialog.callDialog();
     }
+
+
 }
